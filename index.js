099bc5956fbd530f9c1094b715daf47c0ac51f11
Fatemeh sadat Torabi <f.torabi9112@yahoo.com>
2023-03-06 21:31:46 +0000

    I pushed city at sunset main files.

---
+++ index.js
@@ -0,0 +1,12 @@
+const lightWindow = document.querySelector(".lightWindow");
+const groundArea = document.querySelector(".ground");
+const bodym = document.querySelector("body");
+// ಠ_ಠ
+function loadMe(){
+    // bodym.style.height = "200%";
+    const textNew = document.createElement("p");
+    textNew.classList.add("")
+
+
+    
+}
